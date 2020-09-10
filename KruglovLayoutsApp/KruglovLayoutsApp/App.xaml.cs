56e1539a627ec0e84b0a0ee4c3203f760a524557
opilane <opilane@HP-CZC6297B8L>
2020-09-10 10:46:31 +0300

    RGB(Slider)

--- KruglovLayoutsApp/KruglovLayoutsApp/App.xaml.cs
+++ KruglovLayoutsApp/KruglovLayoutsApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace KruglovLayoutsApp
         {
             InitializeComponent();
 
-            MainPage = new Valgusfoor2();
+            MainPage = new rgb();
 
         }
 
---
+++ KruglovLayoutsApp/KruglovLayoutsApp/rgb.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KruglovLayoutsApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class rgb : ContentPage
+    {
+        public rgb()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Editor1.FontSize = Slider1.Value;
+        }
+
+        private void Slider1_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor1.FontSize = e.NewValue;
+            }
+        }
+        private void Slider2_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor1.FontSize = e.NewValue;
+            }
+        }
+        private void Slider3_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor1.FontSize = e.NewValue;
+            }
+        }  
+    }
+}
